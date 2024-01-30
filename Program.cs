df3ed0555804225bd82cf0aee466c1c9dac2a94f
alangorgin <127779778+alangorgin@users.noreply.github.com>
2024-01-30 13:27:57 +0100

    Socket Uppgift 4

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace socket_4;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static readonly HttpClient client = new();
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter ID to fetch a post:");
+            string id = Console.ReadLine() ?? "";
+
+            string responseBody = await GetPostAsync(id);
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException error)
+        {
+            Console.WriteLine("Error: {0}", error.Message);
+        }
+    }
+
+    static async Task<string> GetPostAsync(string id)
+    {
+        string url = $"https://jsonplaceholder.typicode.com/posts/{id}";
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
+}
